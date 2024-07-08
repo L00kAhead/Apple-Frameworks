94e882c076544dd5d4bbf4a0e12ac17b979e5dc0
Himanshu Soni <himanshusoni0925@gmail.com>
2024-07-08 18:11:33 +0700

    Initial Commit

---
+++ Apple Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple Frameworks
+//
+//  Created by Himanshu Soni on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
